d9c6edfd560592344feab1b836fd61cdf21dca57
Daniel Ataíde <117694864+BigJhinZ@users.noreply.github.com>
2024-11-13 15:30:21 -0300

    Delete Backend directory

--- Backend/src/unity/dto/create-unity.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateUnityDto {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio.' })
-  @IsString({ message: 'O nome não pode ser vazio.' })
-  name: string;
-}
